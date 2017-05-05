508741178ff5e66716295e945d2d8826f1617600
Shiming Ge <v-shige@microsoft.com>
2017-05-05 17:57:29 +0800

    tt

---
+++ gcstress-crypto.cmd
@@ -0,0 +1,70 @@
+@if not defined _echo @echo off
+setlocal 
+
+set CONFIGURATIONGROUP=Release
+set ANYOS_ANYCPU_DEBUG_LOCATION=%~dp0\corefx\bin\AnyOS.AnyCPU.%CONFIGURATIONGROUP%
+set WINDOWS_NT_ANYCPU_DEBUG_LOCATION=%~dp0\corefx\bin\Windows_NT.AnyCPU.%CONFIGURATIONGROUP%
+set TESTHOST_PATH=%~dp0\corefx\bin\testhost\netcoreapp-Windows_NT-%CONFIGURATIONGROUP%-x64
+
+@echo TestGCStressLevel : %TestGCStressLevel%
+@echo COMPlus_GCStress : %COMPlus_GCStress%
+
+set ERRORLEVEL=0
+set exitCode=0
+set errorCount=0
+set totalCount=0
+
+call :RunSpecificLibs %ANYOS_ANYCPU_DEBUG_LOCATION%
+call :RunSpecificLibs %WINDOWS_NT_ANYCPU_DEBUG_LOCATION%
+
+@echo Crypto - Total Count: %totalCount%
+@echo Crypto - Error Count: %errorCount%
+exit /b %exitCode%
+
+
+:RunSpecificLibs
+set TARGET_PATH=%~1
+@echo .
+@echo Current Test Files are at %TARGET_PATH%
+@echo .
+
+pushd %TARGET_PATH%
+
+FOR /D %%F IN (System.Security.Crypto*.Tests) DO (
+	IF EXIST %%F\netcoreapp (
+		pushd %%F\netcoreapp
+        @echo Looking in %cd%...
+		IF EXIST RunTests.cmd (
+            set /a totalCount=%totalCount%+1
+			@echo ... found tests
+			CALL RunTests.cmd %TESTHOST_PATH%
+			IF NOT %ERRORLEVEL% == 0 (
+				set /a errorCount=%errorCount%+1
+				set exitCode=%ERRORLEVEL%
+				@echo "error: One or more tests failed while running tests from '%TARGET_PATH%\%%F\netcoreapp'.  Exit code %exitCode%."
+			)
+		)
+		popd
+	)
+	IF EXIST %%F\netstandard (
+		pushd %%F\netstandard
+        @echo Looking in %cd%...
+		IF EXIST RunTests.cmd (
+			set /a totalCount=%totalCount%+1
+            @echo ... found tests
+			CALL RunTests.cmd %TESTHOST_PATH%
+			IF NOT %ERRORLEVEL% == 0 (
+				set /a errorCount=%errorCount%+1
+				set exitCode=%ERRORLEVEL%
+				@echo "error: One or more tests failed while running tests from '%TARGET_PATH%\%%F\netstandard'.  Exit code %exitCode%."
+			)
+		)
+		popd
+	)
+)
+
+popd
+
+goto :EOF
+
+REM Function END
--- gcstress-net-crypto.sh
+++ gcstress-net-crypto.sh
@@ -1,57 +1,52 @@
-#!/bin/bash
-# GC stress for NETWORKING and CRYPTO
-
+#!/usr/bin/env bash
+ConfigurationGroup="Release"
 ProjectRoot="$PWD/corefx/bin"
-#echo project executing path: $ProjectRoot
-ANYOS_ANYCPU_DEBUG="$ProjectRoot/AnyOS.AnyCPU.Debug"
-LINUX_ANYCPU_DEBUG="$ProjectRoot/Linux.AnyCPU.Debug"
-UNIX_ANYCPU_DEBUG="$ProjectRoot/Unix.AnyCPU.Debug"
-TESTHOST_RUNTIME="$ProjectRoot/testhost/netcoreapp-Linux-Debug-x64"
-
+ANYOS_ANYCPU_DEBUG="$ProjectRoot/AnyOS.AnyCPU.$ConfigurationGroup"
+LINUX_ANYCPU_DEBUG="$ProjectRoot/Linux.AnyCPU.$ConfigurationGroup"
+UNIX_ANYCPU_DEBUG="$ProjectRoot/Unix.AnyCPU.$ConfigurationGroup"
+TESTHOST_RUNTIME="$ProjectRoot/testhost/netcoreapp-Linux-$ConfigurationGroup-x64"
 
 exitCode=0
 
 run-tests()
 {
-for testProject in $@
-do
-  dirName="$testProject/netcoreapp"
-  if [ ! -d "$dirName" ]; then
-    dirName="$testProject/netstandard"
-    if [ ! -d "$dirName" ]; then
-      echo "Nothing to test in $dirName"
-      continue
-    fi
-  fi
-  if [ ! -e "$dirName/RunTests.sh" ]; then
-    echo "Cannot find $dirName/RunTests.sh"
-    continue
-  fi
-  pushd $dirName
-  echo "Current workspace: $dirName"
-  echo
-  echo "Running tests in $dirName"
-  echo "./RunTests.sh $TESTHOST_RUNTIME"
-  ./RunTests.sh $TESTHOST_RUNTIME
-  exitCode=$?
-  if [ $exitCode -ne 0 ]
-  then
-      echo "error: One or more tests failed while running tests from '$dirName/RunTests.sh'.  Exit code $exitCode."
-  fi
-  popd
-done
+  for testProject in $@
+  do
+      dirName="$testProject/netcoreapp"
+      if [ ! -d "$dirName" ]; then
+          dirName="$testProject/netstandard"
+          if [ ! -d "$dirName" ]; then
+              echo "Nothing to test in $dirName"
+              continue
+          fi
+      fi
+      if [ ! -e "$dirName/RunTests.sh" ]; then
+          echo "Cannot find $dirName/RunTests.sh"
+          continue
+      fi
+      pushd $dirName
+      echo "Current workspace: $dirName"
+      echo
+      echo "Running tests in $dirName"
+      echo "./RunTests.sh $TESTHOST_RUNTIME"
+      ./RunTests.sh $TESTHOST_RUNTIME
+      exitCode=$?
+      if [ $exitCode -ne 0 ]
+      then
+          echo "error: One or more tests failed while running tests from '$dirName/RunTests.sh'.  Exit code $exitCode."
+      fi
+      popd
+  done
 }
 
-run-tests $ANYOS_ANYCPU_DEBUG/System.Net.*.Tests
-run-tests $LINUX_ANYCPU_DEBUG/System.Net.*.Tests
-run-tests $UNIX_ANYCPU_DEBUG/System.Net.*.Tests
-
-run-tests $ANYOS_ANYCPU_DEBUG/System.Security.Crypto*.Tests
-run-tests $LINUX_ANYCPU_DEBUG/System.Security.Crypto*.Tests
-run-tests $UNIX_ANYCPU_DEBUG/System.Security.Crypto*.Tests
-
-run-tests $ANYOS_ANYCPU_DEBUG/System.Console.Tests
-run-tests $LINUX_ANYCPU_DEBUG/System.Console.Tests
-run-tests $UNIX_ANYCPU_DEBUG/System.Console.Tests
+run-tests "$ANYOS_ANYCPU_DEBUG/System.Net.*.Tests"
+run-tests "$LINUX_ANYCPU_DEBUG/System.Net.*.Tests"
+run-tests "$UNIX_ANYCPU_DEBUG/System.Net.*.Tests"
+run-tests "$ANYOS_ANYCPU_DEBUG/System.Security.Crypto*.Tests"
+run-tests "$LINUX_ANYCPU_DEBUG/System.Security.Crypto*.Tests"
+run-tests "$UNIX_ANYCPU_DEBUG/System.Security.Crypto*.Tests"
+run-tests "$ANYOS_ANYCPU_DEBUG/System.Console.Tests"
+run-tests "$LINUX_ANYCPU_DEBUG/System.Console.Tests"
+run-tests "$UNIX_ANYCPU_DEBUG/System.Console.Tests"
 
 exit $exitCode
